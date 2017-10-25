14e958ee747d2f16fd9b72e018e19cc386d1bf7a
israel <israel@waveapplication.com>
2017-10-25 12:56:08 +0200

    commit with both graphics

---
+++ Ejercicio2/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#boton-ajax").click(function create_graphics(){
+        $("#ajax-load").load('graphics.html');
+        $("#ajax-load_2").load('graphics_2.html');
+    });
+});
